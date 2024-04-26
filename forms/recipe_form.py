60e8bdd53945b9fc822f4cbf14c6bbd2bb4f23d0
Tapok <veronikasheh19.12@gmail.com>
2024-04-26 10:58:12 +1000

    Finish forms

--- forms/recipe_form.py
+++ forms/recipe_form.py
@@ -3,15 +3,9 @@ from wtforms import StringField, TextAreaField, BooleanField, SubmitField, FileF
 from wtforms.validators import DataRequired
 
 
-# class Ingredients(FlaskForm):
-#     ingredient_name = StringField('Название ингредиента', validators=[DataRequired()])
-#     ingredient_value = IntegerField('Количество', validators=[DataRequired()])
-
-
 class RecipeForm(FlaskForm):
     title = StringField('Заголовок', validators=[DataRequired()])
     dish_image = FileField('Картинка')
-    # ingredients = FieldList(Ingredients('Ингредиенты'), validators=[DataRequired()], min_entries=1)
     content = TextAreaField('Содержание', validators=[DataRequired()])
     is_private = BooleanField('Личное')
     submit = SubmitField('Применить')
